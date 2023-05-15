ca0079d67b554420dac75d51605547e191a40314
s.nakamura <shotenba@gmail.com>
2023-05-16 00:53:39 +0900

    コンパイルをcommonJS→EC6に変更

--- dist/food.js
+++ dist/food.js
@@ -1,9 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Food = void 0;
-const foods_js_1 = require("./foods.js");
-const score_js_1 = require("./score.js");
-class Food {
+import { Foods } from "./foods.js";
+import { Score } from "./score.js";
+export class Food {
     constructor(element) {
         this.element = element;
         element.addEventListener('click', this.clickEventHandler.bind(this));
@@ -11,12 +8,11 @@ class Food {
     clickEventHandler() {
         console.log('aaaaa');
         this.element.classList.toggle('food--active');
-        const foods = foods_js_1.Foods.getInstance();
-        const score = score_js_1.Score.getInstance();
+        const foods = Foods.getInstance();
+        const score = Score.getInstance();
         foods.activeElements;
         foods.activeElementsScore;
         score.totalScore;
         score.render();
     }
 }
-exports.Food = Food;
